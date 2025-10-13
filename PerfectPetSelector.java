20f50d337ea6d021d47e76652e0a46764d8179d1
melinozturk <MelinOzturk0707@gmail.com>
2025-10-13 15:02:30 +0000

    start

--- PerfectPetSelector.java
+++ PerfectPetSelector.java
@@ -1,1 +1,31 @@
+import java.util.*;
+public class PerfectPetSelector{
 
+    public static void main(String [] args){
+        try (// get user input
+        Scanner scanner = new Scanner(System.in)) {
+            String userName;
+            
+            // Enter username and press Enter
+            System.out.println("Enter username"); 
+            userName = scanner.nextLine();   
+               
+            System.out.println("Username is: " + userName);
+        }
+        
+    String color = "blue";
+        String season = "fall";
+        if (color.equals("blue"));
+        if (season.equals("fall"));
+        //make sure it's valid
+        
+        System.out.println("Alligator is your perfect pet!");
+
+        //make sure it's valid
+       
+       
+
+        //calculate the user's perfect PetSelector
+        //output the decision
+    }
+}
